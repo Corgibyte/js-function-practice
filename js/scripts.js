2a1bbe0fabbc7f93f2078e6494af91f6fab3c9d9
Hannah Y <hannah@corgibyte.com>
2021-10-18 13:45:07 -0700

    Add additional encode functions.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-authored-by: Nick Benzinger <nicholasbenzinger@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,21 @@ function encode(original) {
   return original.charAt(0).toUpperCase() + original.charAt(original.length - 1).toUpperCase();
 }
 
+function encodeFlip(original) {
+  return original.charAt(1) + original.charAt(0);
+}
+
+function encodeAndFlip(original) {
+  return original + encodeFlip(encode(original));
+}
+
+function counter(original) {
+  return original.charAt(parseInt(original.length/2)) + encodeAndFlip(original);
+}
+
+function reverse(original){
+  return original.split("").reverse().join("");
+}
+
 const sentence = prompt("Enter a sentence:");
-alert(encode(sentence));
+alert(reverse(counter(sentence)));
